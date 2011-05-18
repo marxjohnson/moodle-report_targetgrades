27fa36a4b4c66ac4c737e53ebe6e7a21f4f83e17
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-05-18 09:04:21 +0100

    Changed database table names

--- db/upgrade.php
+++ db/upgrade.php
@@ -43,9 +43,36 @@ function xmldb_block_mtgdistribute_upgrade($oldversion=0) {
         if ($scale = $DB->get_record('scale', array('name' => ALIS_ADVANCED_SUBSIDIARY_GCE_DOUBLE.' MTG'))) {
             $result = $result && $DB->set_field('scale', array('scale' => MTG_SCALE_ADVANCED_SUBSIDIARY_GCE_DOUBLE, 'name' => ALIS_ADVANCED_SUBSIDIARY_GCE_DOUBLE.' MTG'));
         }
-
-        return $result;
     }
+
+	if ($result && $oldversion < 2011051700) {
+		// Moodle 2 upgrade, changing to a report and changing the table names.
+		// Define table mtgdistribute to be dropped
+        $table = new xmldb_table('mtgdistribute');
+
+        // Conditionally launch drop table for mtgdistribute
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
+        }
+		
+		$table = new xmldb_table('mtgdistribute_alisdata');
+
+        // Launch rename table for mtgdistribute
+        $dbman->rename_table($table, 'report_targetgrades_alisdata');
+		
+		$table = new xmldb_table('mtgdistribute_qualtype');
+
+        // Launch rename table for mtgdistribute
+        $dbman->rename_table($table, 'report_targetgrades_qualtype');
+		
+		$table = new xmldb_table('mtgdistribute_patterns');
+
+        // Launch rename table for mtgdistribute
+        $dbman->rename_table($table, 'report_targetgrades_patterns');
+		
+    }
+
+    return $result;
 }
 
 ?>
